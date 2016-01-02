db8ee5cff39db63dbaf798cd9633f00d507e3b87
TheYellowKrato <azamanama@gmail.com>
2016-01-02 23:06:20 +0100

    Add Q LaneClear if it will hit 2 minions

--- Ninja Bard/Modes/LaneClear.cs
+++ Ninja Bard/Modes/LaneClear.cs
@@ -1,4 +1,6 @@
 ﻿using EloBuddy.SDK;
+using SharpDX;
+using System.Collections.Generic;
 
 namespace Bard.Modes
 {
@@ -11,7 +13,30 @@ namespace Bard.Modes
 
         public override void Execute()
         {
+            Farm();
+        }
+        public void Farm()
+        {
+            var minions = Orbwalker.LaneclearMinions.FindAll(m => m.IsValidTarget(Q.Range));
 
+            if (minions.Count <= 1)
+            {
+                return;
+            }
+            if (Q.IsReady() && Config.Modes.JungleClear.UseQ)
+            {
+                var minion = minions[0];
+                foreach (var m in minions)
+                {
+                    var pred = Q.GetPrediction(m);
+
+                    if (pred.CollisionObjects.Length >= 1)
+                    {
+                        Q.Cast(m);
+                        return;
+                    }
+                }
+            }
         }
     }
 }
