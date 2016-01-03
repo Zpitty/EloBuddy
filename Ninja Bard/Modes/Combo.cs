b8e2b9c6b7c3a85fd37de7ac24bbe77b6061115e
Zpitty <npuetz50@gmail.com>
2016-01-03 05:03:49 -0600

    tweak

--- Ninja Bard/Modes/Combo.cs
+++ Ninja Bard/Modes/Combo.cs
@@ -32,28 +32,24 @@ namespace Bard.Modes
 
                 if (target == null) { return; }
 
-
                 if (target != null && target.IsValid && Misc.WallBangable(target) && predictionQ.CollisionObjects.Count() == 0)
                 {
                     Q.Cast(predictionQ.CastPosition);
                     return;
                 }
-
                 //else if (predictionQ.CollisionObjects.Count() == 1)
                 //{
                 //    Q.Cast(predictionQ.CastPosition);
                 //    return;
                 //}
 
-                if (Qext == null)
-                { return; }
-
+                //if (Qext == null)
+                //{ return; }
                 else if (predQext.CollisionObjects.Count(a => a.IsValidTarget(Q.Range) && a.Distance(Qext) <= Settings.QBindDistanceM) == 1)
                 {
                     Q.Cast(predQext.CastPosition);
                     return;
                 }
-
             }
             #endregion
         }
